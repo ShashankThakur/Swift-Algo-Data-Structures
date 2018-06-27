602cf44e6d4e18f0c188030a15141e09f58230cd
Shashank Thakur <shashank.thakur@disney.com>
2018-06-26 22:32:47 -0700

    Added solution for LCA for binary tree

---
+++ LeastCommonAncestorBianryTree.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+class BinaryNode<T:Comparable> {
+    public var left:BinaryNode?
+    public var right:BinaryNode?
+    public var val:T
+    public init(val:T){
+        self.val = val
+    }
+    static public func == (lhs:BinaryNode<T>,rhs:BinaryNode<T>) ->Bool{
+        return lhs.val == rhs.val
+    }
+}
+
+
+func LCA<T>(_ root:BinaryNode<T>?,_ n1:BinaryNode<T>, _ n2: BinaryNode<T>) ->BinaryNode<T>? {
+    if(root == nil) {
+        return nil
+    }
+    if(root! == n1 || root! == n2) {
+        return root
+    }
+    let left = LCA(root!.left, n1, n2)
+    let right = LCA(root!.right, n1, n2)
+    if(left != nil && right != nil) {
+        return root
+    }
+    if(left == nil && right == nil) {
+        return nil
+    }
+    return left != nil ? left:right
+}
+

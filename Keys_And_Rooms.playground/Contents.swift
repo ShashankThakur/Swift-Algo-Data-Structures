e9f04feb1995c45357933ddbd314a91a16157f90
Shashank Thakur <shashank.thakur@disney.com>
2018-07-11 16:59:46 -0700

    Added solution for room and key problem

---
+++ Keys_And_Rooms.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
+ 
+ Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+ 
+ Initially, all the rooms start locked (except for room 0).
+ 
+ You can walk back and forth between rooms freely.
+ 
+ Return true if and only if you can enter every room.
+ 
+ Example 1:
+ 
+ Input: [[1],[2],[3],[]]
+ Output: true
+ Explanation:
+ We start in room 0, and pick up key 1.
+ We then go to room 1, and pick up key 2.
+ We then go to room 2, and pick up key 3.
+ We then go to room 3.  Since we were able to go to every room, we return true.
+ Example 2:
+ 
+ Input: [[1,3],[3,0,1],[2],[0]]
+ Output: false
+ Explanation: We can't enter the room with number 2.
+ */
+//Complexity O(n * n)
+
+class Solution {
+    func canVisitAllRooms(_ rooms: [[Int]]) -> Bool {
+        var enteredRoomSet:Set<Int> = []
+        helper(rooms, 0, &enteredRoomSet)
+        if(enteredRoomSet.count == rooms.count) {
+            return true
+        }
+        return false
+    }
+    func helper(_ rooms:[[Int]], _ index:Int, _ enteredRoomSet:inout Set<Int>) {
+        enteredRoomSet.insert(index)
+        for key in rooms[index] {
+            if(enteredRoomSet.contains(key)){
+                continue
+            }
+            helper(rooms,key,&enteredRoomSet)
+        }
+    }
+}
